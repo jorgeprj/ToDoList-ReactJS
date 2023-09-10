c8cbcbe1035cf0e42d025db72fe3db95162882ce
jorgeprj <jlmp2002@gmail.com>
2023-09-10 16:31:09 -0300

    feat: app structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,44 @@ import { useState } from 'react'
 
 
 function App() {
-
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'New Features',
+      category: 'Job',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: 'Learning React Components',
+      category: 'Study',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: 'Supermarket',
+      category: 'Personal',
+      isCompleted: false
+    }
+  ])
   return (
-    <div>React</div>
+    <div className='app'>
+      <h1>To Do List</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <div>
+            <div className='content'>
+              <p>{todo.title}</p>
+              <p className='category'>{(todo.category)}</p>
+            </div>
+            <div>
+              <button>Completed</button>
+              <button>X</button>
+            </div>
+          </div>
+         ))}
+      </div>
+    </div>
   )
 }
 
