ca1773634715e5878039886b2e9253f5620ee9b1
jorgeprj <jlmp2002@gmail.com>
2023-09-10 17:46:09 -0300

    feat: complete and remove buttons

--- src/App.jsx
+++ src/App.jsx
@@ -42,12 +42,24 @@ function App() {
 		setTodos(newTodo);
 	}
 
+	const removeTodo = (id) => {
+		const newTodos = [...todos]
+		const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+        setTodos(filteredTodos);
+	}
+
+	const completeTodo = (id) => {
+		const newTodos = [...todos]
+        newTodos.map(todo => todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo);
+        setTodos(newTodos);
+	}
+
 	return (
 		<div className='app'>
 			<h1>To Do List</h1>
 			<div className='todo-list'>
 				{todos.map((todo) => (
-					<Todo key={todo.id} todo={todo} />
+					<Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
 				))}
 			</div>
 			<div>
